d8fb8c4cd91e529198fcf1a4f79be2dca3bc3933
Odane Williams <odanew1@gmail.com>
2021-10-10 23:32:20 -0500

    Updated tic-tac-toe.js with ex ersise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,30 @@
 window.onload = function(){
     var cDiv = document.getElementById("board").childNodes;
-    cDiv.forEach((div) => div.className = "square");
-}
-
+    cDiv.forEach((div) => div.className = "square"); 
     
+    const alt = [];
+    cDiv.forEach((div) => div.addEventListener("click", function(){
+      
+      if (div.className == "square" && alt[alt.length-1] != "X"){         
+         div.className = "square X";
+         div.textContent = "X";
+         alt.push("X");            
+            
+      }
+         
+      else if(div.className == "square" && alt[alt.length-1] != "O"){
+            div.className = "square O";
+            div.textContent = "O";
+            alt.push("O");
+               
+               
+               
+      }
+            
+   })); 
+      
+      
+      alert("Ran")
 
+   }
+    
